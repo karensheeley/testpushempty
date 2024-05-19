942f9dc4678fb6b5ac442c5f9b23b0934d4c433c
Karen Sheeley <karen.sheeley@sdpstl.com>
2024-05-19 09:42:13 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+
+Console.WriteLine("Hello, World!");
+
+var input = "2022-07-29T00:00:00-04:00";
+
+var input2 = "4/12/2024";
+
+
+//var output = DateTime.Parse(input);
+//var output = DateTime.ParseExact(input);
+
+
+var outputx = DateTime.Parse(input);
+var output = DateTimeOffset.Parse(input);
+var output2 = DateTimeOffset.ParseExact(input, "yyyy-MM-ddTHH:mm:sszzzz", System.Globalization.CultureInfo.InvariantCulture);
+
+var outputr = output.DateTime;
+var output2r = output2.DateTime;
+
+var out2 = DateTime.Parse(input2);
+
+
+
+Console.WriteLine(outputx.ToString());
+Console.WriteLine(outputr.ToString());
+Console.WriteLine(output2r.ToString());
+
+
+Console.WriteLine(output.Date.ToString("MM/dd/yyyy"));
+
+
+Console.ReadLine();
+
